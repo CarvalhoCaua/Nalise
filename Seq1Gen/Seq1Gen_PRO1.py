9725207eee788926f70df0167932bf37d1bbfda4
Cauã Carvalho <cauacarvalho.ferreirarezende@gmail.com>
2024-12-17 17:31:54 -0300

    Teste

---
+++ Seq1Gen/Seq1Gen_PRO1.py
@@ -0,0 +1,35 @@
+#Análise simples de uma sequência básica de DNA
+
+gene = "GATGGAACTTGACTACGTAAATT"
+
+print("Sequência do Gene: {}".format(gene))
+print("Tamanho do Gene: {}".format(len(gene)))
+print("Primeiras Três Bases: {}".format(gene[:3]))
+print("Últimas Três Bases: {}".format(gene[-3:]))
+print("Quantidades de Adenina (A): {}".format(gene.count("A")))
+print("Quantidades de Timina (T): {}".format(gene.count("T")))
+print("Quantidades de Guanina (G): {}".format(gene.count("G")))
+print("Quantidades de Citosina (C): {}".format(gene.count("C")))
+genec = ''
+for i in gene:
+    if i == "A":
+        genec += "T"
+    elif i == "T":
+        genec += "A"
+    elif i == "C":
+        genec += "G"
+    elif i == "G":
+        genec += "C"
+print(f"Sequência Complementar: {genec}")
+print(f"Quantidade de Ligações GCs: {gene.count('G')}")
+rnaseq = ''
+for i in gene:
+    if i == "A":
+        rnaseq += "A"
+    elif i == "T":
+        rnaseq += "U"
+    elif i == "C":
+        rnaseq += "G"
+    elif i == "G":
+        rnaseq += "C"
+print(f"Sequência Transcrita (RNA): {rnaseq}")
---
+++ Seq1Gen/Seq1Gen_PRO3.py
@@ -0,0 +1,5 @@
+#Teste da Função do PRO2
+
+from Seq1Gen_PRO2 import analise
+
+analise("GATGGAACTTGACTACGTAAATT")
